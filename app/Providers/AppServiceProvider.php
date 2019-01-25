1c00edb200cbf7e0cba3def8d795d2a6667f81c4
Eli MArshall <elimarshall@Elis-Mac-mini.local>
2019-01-24 22:31:19 -0500

    initial commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\Schema;
+// use App\Providers\Schema;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -13,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 
     /**
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,108 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('content')
+  <div class="" id="hero">
+    <h3 id="hero-top" class="text-center col-xs-12">Full Service</h3>
+    <h2 id="hero-middle" class="text-center col-xs-12">MOBILE CAR WASH</h2>
+    <h3 id="hero-bottom" class="text-center col-xs-12">Car Care Specialists</h3>
+    <button class="btn btn-info btn-lg col-xs-2 col-xs-offset-5">Tell Me More</button>
+  </div>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+  <div class="section" id="services">
+    <h2 class="col-xs-12 text-center">Services</h2>
+    <div class="container">
+      <div class="col-sm-4 service-section">
+        <i class="fa fa-car fa-3x col-xs-12 text-center" aria-hidden="true"></i>
+        <h4 class="text-center">Auto / Truck / SUV <br> Motorcycles / Boats / RV's</h4>
+      </div>
+      <div class="col-sm-4 service-section">
+        <i class="fa fa-money fa-3x col-xs-12 text-center" aria-hidden="true"></i>
+        <h4 class="text-center">Affordable Pricing <br> & <br> Great Service</h4>
+      </div>
+      <div class="col-sm-4 service-section">
+        <i class="fa fa-fighter-jet fa-3x col-xs-12 text-center" aria-hidden="true"></i>
+        <h4 class="text-center">Stay Where You're At! <br> We Come To You!</h4>
+      </div>
+    </div>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
 
-            .full-height {
-                height: 100vh;
-            }
+  </div>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+  <div class="section" id="pricing">
+    <h2 class="col-xs-12 text-center">Pricing</h2>
+    <div class="container">
+      <div class="col-sm-6 pricing-section">
+        <h3 class="text-center col-xs-12">$20</h3>
+        <h4 class="text-center col-xs-12">Exterior Car Wash</h4>
+        <h4 class="text-center col-xs-12">Interior Detailing</h4>
+        <h4 class="text-center col-xs-12">Window Cleaning</h4>
+        <h4 class="text-center col-xs-12">Bug Grille Cleaning</h4>
+      </div>
+      <div class="col-sm-6 pricing-section">
+        <h3 class="text-center col-xs-12">$5 Add Ons (Each)</h3>
+        <h4 class="text-center col-xs-12">Hand Wax</h4>
+        <h4 class="text-center col-xs-12">Interior Dressing/Mat Shampoo</h4>
+        <h4 class="text-center col-xs-12">Leather/Cloth Seat Cleaning</h4>
+        <h4 class="text-center col-xs-12">Carpet Spot Cleaning</h4>
+      </div>
+    </div>
+  </div>
 
-            .position-ref {
-                position: relative;
-            }
+  <div class="section" id="about">
+    <h2 class="col-xs-12 text-center">About</h2>
+    <div class="container">
+      <p class="col-xs-12 text-center">We take pride in what we do here at High Class Mobile Car Wash and pay extra attention to detail.<br><br>We like to provide an easy and affordable way to get your car washed so you can be proud of your ride. Call us and we can make sure you turn heads next time you take out your car!</p>
+    </div>
+  </div>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+  <div class="section" id="team">
+    <h2 class="col-xs-12 text-center">Team</h2>
+    <div class="container">
+      <div class="col-sm-4">
+        <div class="team-card col-xs-12">
+          <h4 class="col-xs-12 text-center">Name:</h4>
+          <h4 class="col-xs-12 text-center">(555) 555-5555</h4>
         </div>
-    </body>
-</html>
+      </div>
+      <div class="col-sm-4">
+        <div class="team-card col-xs-12">
+          <h4 class="col-xs-12 text-center">Name:</h4>
+          <h4 class="col-xs-12 text-center">(555) 555-5555</h4>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="team-card col-xs-12">
+          <h4 class="col-xs-12 text-center">Name:</h4>
+          <h4 class="col-xs-12 text-center">(555) 555-5555</h4>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="section" id="gallery">
+    <h2 class="col-xs-12 text-center">Gallery</h2>
+  </div>
+
+  <div class="section" id="contact">
+    <h2 class="col-xs-12 text-center">Contact</h2>
+
+    <h4 class="col-xs-12 text-center">Highclassmobile5@gmail.com</h4>
+    <div class="container">
+      <div class="col-sm-4">
+        <a href="https://www.instagram.com/highclassmobilecarwash/" target="_blank"><i class="fa fa-instagram fa-3x text-center col-xs-12" id="instagram" aria-hidden="true"></i></a>
+      </div>
+      <div class="col-sm-4">
+        <a href="https://www.facebook.com/highclass.mobilecarwash" target="_blank"><i class="fa fa-facebook fa-3x text-center col-xs-12" id="facebook" aria-hidden="true"></i></a>
+      </div>
+      <div class="col-sm-4">
+          <a href="http://www.snapchat.com/add/highclassmobilecarwash" target="_blank"><i class="fa fa-snapchat fa-3x text-center col-xs-12" id="snapchat" aria-hidden="true"></i></a>
+      </div>
+    </div>
+
+
+  </div>
+  <div class="" id="bottom-footer">
+
+  </div>
+
+@endsection
